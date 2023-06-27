4c7d062dcc76f2c725a461929649207462ad5bec
Wilson <sajuvez.1996@gmail.com>
2023-06-27 11:30:49 -0500

    Initial Commit

---
+++ PokeApiWT/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PokeApiWT
+//
+//  Created by Wilson Jair Tique Aguja on 27/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokeApiWT/PokeApiWTApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeApiWTApp.swift
+//  PokeApiWT
+//
+//  Created by Wilson Jair Tique Aguja on 27/06/23.
+//
+
+import SwiftUI
+
+@main
+struct PokeApiWTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
